57b9557c774fc33e10797b22508463731cada735
XxThunderBlastxX <koustavmondal55@gmail.com>
2021-11-26 21:39:06 +0530

    made grid view

---
+++ lib/categories_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+import 'category_items.dart';
+import 'dummy_data.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("DeliMeal"),
+      ),
+      body: GridView(
+        children: dummyCategories
+            .map((cData) => CategoryItem(cData.title, cData.color))
+            .toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+      ),
+    );
+  }
+}
