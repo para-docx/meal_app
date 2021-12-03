38e35a959d01d5dd21dcdcdfc46fe38d18a34216
XxThunderBlastxX <koustavmondal55@gmail.com>
2021-12-04 03:04:36 +0530

    unknownRoute added

--- lib/main.dart
+++ lib/main.dart
@@ -39,6 +39,11 @@ class MyApp extends StatelessWidget {
         CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
         MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
       },
+      onUnknownRoute: (setting) {
+        return MaterialPageRoute(
+          builder: (ctx) => CategoriesScreen(),
+        );
+      },
     );
   }
 }
